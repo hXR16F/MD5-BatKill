d61495448842ec52393fa5c69f30cacaa4bf9771
hXR16F <hxr16f.ar@gmail.com>
2020-04-23 18:12:37 +0200

    Add files via upload

--- scripts/hashkiller2batkill.py
+++ scripts/hashkiller2batkill.py
@@ -1,5 +1,7 @@
+wordlist = 'my-wordlist.txt'
+
 with open('dumpik.txt', 'w') as f:
-	for line in open('360707.txt', 'r'):
+	for line in open(wordlist, 'r'):
 		hash, word = line.split(':')
 		print(f'{hash[:4]}:{word}', end ="", file=f)
         
--- scripts/wordlist2batkill.py
+++ scripts/wordlist2batkill.py
@@ -1,6 +1,8 @@
+wordlist = 'my-wordlist.txt'
+
 from hashlib import md5
 with open('dumpik.txt', 'w') as f:
-	for line in open('1000000-password-seclists.txt', 'r'):
+	for line in open(wordlist, 'r'):
 		result = md5(line.encode()).hexdigest()
 		print(f'{result[:4]}:{line}', end ="", file=f)
         
