793cbe497a2e65c01a297060c823d79fb15904bc
hXR16F <hxr16f.ar@gmail.com>
2020-06-19 13:28:56 +0200

    Add files via upload

--- scripts/hashkiller2batkill.py
+++ scripts/hashkiller2batkill.py
@@ -1,7 +1,14 @@
 wordlist = 'my-wordlist.txt'
+wordlist_save_as = "dump.txt"
 
-with open('dumpik.txt', 'w') as f:
-	for line in open(wordlist, 'r'):
-		hash, word = line.split(':')
-		print(f'{hash[:4]}:{word}', end ="", file=f)
-        
+errors = 0
+with open(wordlist_save_as, 'w') as f:
+    for line in open(wordlist, 'r'):
+        try:
+            hash, word = line.split(':')
+            print(f'{hash[:4]}:{word}', end="", file=f)
+        except ValueError:
+            errors += 1
+
+print(f'Saved as: {wordlist_save_as}')
+print(f'Total errors: {errors}')
--- scripts/wordlist2batkill.py
+++ scripts/wordlist2batkill.py
@@ -1,8 +1,12 @@
-wordlist = 'my-wordlist.txt'
-
 from hashlib import md5
-with open('dumpik.txt', 'w') as f:
-	for line in open(wordlist, 'r'):
-		result = md5(line.encode()).hexdigest()
-		print(f'{result[:4]}:{line}', end ="", file=f)
-        
+wordlist = 'my-wordlist.txt'
+wordlist_save_as = "dump.txt"
+
+errors = 0
+with open(wordlist_save_as, 'w') as f:
+    for line in open(wordlist, 'r'):
+        line_parsed = line.replace("\n", "")
+        result = md5(line_parsed.encode()).hexdigest()
+        print(f'{result[:4]}:{line}', end="", file=f)
+
+print(f'Saved as: {wordlist_save_as}')
